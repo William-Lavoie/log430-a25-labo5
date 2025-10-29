fbd2a2a6a1003ec0f99287a8e886ee5be8bee5b3
William-Lavoie <william.lavoie.3@ens.etsmtl.ca>
2025-10-28 23:02:14 -0400

    probleme variables env

--- src/config.py
+++ src/config.py
@@ -13,7 +13,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = int(os.getenv("DB_PORT"))
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PASSWORD = os.getenv("DB_PASS")
 
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = int(os.getenv("REDIS_PORT"))
